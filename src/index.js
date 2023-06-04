cdf99aae434132ddb878669b8a8afa9667f337e0
Abildaev Dastan <abildaev1996@gmail.com>
2023-06-04 23:36:12 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style/index.scss'
+import {createElements} from "./js/elements";
+import {elementAddEventListener} from "./js/functions";
+
+
+
+
+createElements()
+elementAddEventListener()
+
